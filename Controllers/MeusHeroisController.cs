5ba608a31379358a39ecd742a0d2e4334989cc0e
David Tigre Moraes <david.tigre@lambda3.com.br>
2022-10-03 23:38:29 -0300

    Adição nova rota e Solution

---
+++ Controllers/MeusHeroisController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHerois.Controllers
+{
+    public class MeusHeroisController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
